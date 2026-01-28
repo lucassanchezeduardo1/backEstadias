174182add5d54da8510023ab5ef0535418976685
EDUARDO <lucaseduar2008@gmail.com>
2026-01-28 11:54:40 -0600

    creando dtos de la tabla publicaciones

--- src/publicacion/dto/update-publicacion.dto.ts
+++ src/publicacion/dto/update-publicacion.dto.ts
@@ -1,4 +1,50 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreatePublicacionDto } from './create-publicacion.dto';
+import { Type } from "class-transformer";
+import { IsInt,IsOptional, IsString, IsUrl, MaxLength, MinLength } from "class-validator"
+export class UpdatePublicacionDto {
+    @IsString()
+    @IsOptional()
+    @MinLength(5)
+    @MaxLength(255)
+    titulo: string;
 
-export class UpdatePublicacionDto extends PartialType(CreatePublicacionDto) {}
+    @IsInt()
+    @IsOptional()
+    @Type(() => Number)
+    categoria_id: number;
+
+    @IsString()
+    @IsOptional()
+    @MinLength(3)
+    @MaxLength(100)
+    sub_categoria: string;
+
+    @IsString()
+    @IsOptional()
+    @MinLength(5)
+    @MaxLength(500)
+    img_portada_url: string;
+
+    @IsString()
+    @IsOptional()
+    colaboradores: string;
+
+    @IsString()
+    @IsOptional()
+    @MinLength(500)
+    sintesis_investigador: string;
+
+    @IsString()
+    @IsOptional()
+    @MinLength(5)
+    @MaxLength(500)
+    @IsUrl({}, { message: 'La URL del PDF no es válida' })
+    pdf_url: string;
+
+    @IsString()
+    @IsOptional()
+    links_referencia: string;
+
+    @IsString()
+    @IsOptional()
+    videos_url: string;
+}

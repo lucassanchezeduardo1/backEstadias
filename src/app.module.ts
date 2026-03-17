6040597df6538ee838daa480cc1399e0191822b4
EDUARDO <lucaseduar2008@gmail.com>
2026-03-17 13:10:10 -0600

    config de bd

--- src/app.module.ts
+++ src/app.module.ts
@@ -25,7 +25,7 @@ import { GoogleDriveModule } from './google-drive/google-drive.module';
   host: process.env.DB_HOST || 'mysql.railway.internal',
   port: parseInt(process.env.DB_PORT || '3306', 10),
   username: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
+  password: process.env.DB_PASSWORD || 'NsSWOFNuadJcslliirpFyfDBnUTtvfhQ',
   database: process.env.DB_NAME || 'railway',
   autoLoadEntities: true,
   synchronize: true,
